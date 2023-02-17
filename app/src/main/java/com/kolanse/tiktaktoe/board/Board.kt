7b205dbecdad00e87153e40d95992ed4dc019435
samuel <esambooks@gmail.com>
2023-02-17 22:09:18 +0100

    added winning condition check

--- app/src/main/java/com/kolanse/tiktaktoe/board/Board.kt
+++ app/src/main/java/com/kolanse/tiktaktoe/board/Board.kt
@@ -16,9 +16,17 @@ class Board {
     private val _tilesFlow = MutableStateFlow<MutableList<TileState>>(mutableListOf())
     val tilesFlow: StateFlow<MutableList<TileState>> = _tilesFlow
 
-    private val boardState = BoardState.GAME_ON
+
+    private val _boardStatus = MutableStateFlow<BoardState>(BoardState.GAME_ON)
+    val boardStatus: StateFlow<BoardState> = _boardStatus
 
      val tiles = mutableListOf<TileState>()
+
+    private val winningConditions = listOf(
+        listOf(0,1,2), listOf(3,4,5), listOf(6,7,8),
+        listOf(0,3,6), listOf(1,4,7), listOf(2, 5, 8),
+        listOf(0, 4, 8), listOf(2, 4, 6)
+    )
 
 
     init {
@@ -35,8 +43,6 @@ class Board {
         if (tilePlay.gamePlayed != TileState.EMPTY){
             if (tiles[tilePlay.boardPosition.ordinal] == TileState.EMPTY){
                 tiles.add(tilePlay.boardPosition.ordinal, TileState.O)
-
-                System.out.println("the tiles first $tiles")
                 _tilesFlow.value = tiles
             }
         }
@@ -44,6 +50,53 @@ class Board {
     }
 
 
+    /**
+     * This checks the Board status and updates it
+     */
+    private fun checkBoardStatus(){
+
+
+       val xBoardPositions = tiles.withIndex().filter { it.value == TileState.X }.map { it.index }
+        val oBoardPositions = tiles.withIndex().filter { it.value == TileState.O }.map { it.index }
+        val emptyBoardPositions = tiles.withIndex().filter { it.value == TileState.EMPTY }.map { it.index }
+
+        if (xBoardPositions.matchesWinningCombinations()){
+            _boardStatus.value = BoardState.X_WINS
+            return
+        }
+
+        if (oBoardPositions.matchesWinningCombinations()){
+            _boardStatus.value = BoardState.O_WINS
+            return
+        }
+
+
+        if (emptyBoardPositions.isEmpty()){
+            _boardStatus.value = BoardState.DRAW
+            return
+        }
+
+
+    }
+
+
+    /**
+     * Checks to see whether board positions matches winning combination
+     */
+    private fun List<Int>.matchesWinningCombinations(): Boolean{
+
+        if (this.size != 3){
+          return false
+        }
+
+       winningConditions.forEach {
+           if (it.containsAll(this)){
+               return true
+           }
+       }
+
+        return false
+    }
 
 
 
--- app/src/test/java/com/kolanse/tiktaktoe/Board/BoardClassTest.kt
+++ app/src/test/java/com/kolanse/tiktaktoe/Board/BoardClassTest.kt
@@ -21,10 +21,21 @@ class BoardClassTest {
 
 
     @Test
-    fun `assert that a a game play stores successfully in the correct board`() {
+    fun `assert that a  game play stores successfully in the correct board`() {
         val board = Board()
 
         board.playTile(TilePlay(BoardPosition.FIVE, TileState.O))
+        val tiles = board.tilesFlow.value
+
+        Assert.assertEquals(TileState.O, tiles.get(4))
+    }
+
+    @Test
+    fun `assert that a tile already played can't be changed`() {
+        val board = Board()
+
+        board.playTile(TilePlay(BoardPosition.FIVE, TileState.O))
+        board.playTile(TilePlay(BoardPosition.FIVE, TileState.X))
         val tiles = board.tilesFlow.value
 
         Assert.assertEquals(TileState.O, tiles.get(4))
